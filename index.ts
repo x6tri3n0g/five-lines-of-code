0a2b2bf1d854d9face635a2b99f32282068de999
xtring <hh940630@gmail.com>
2023-06-19 01:24:07 +0900

    update 함수의 코드를 handleInputs, updateMap 함수로 추출하라

--- index.ts
+++ index.ts
@@ -80,7 +80,7 @@ function moveVertical(dy: number) {
   }
 }
 
-function update() {
+function handleInputs() {
   while (inputs.length > 0) {
     let current = inputs.pop();
     if (current === Input.LEFT)
@@ -92,7 +92,9 @@ function update() {
     else if (current === Input.DOWN)
       moveVertical(1);
   }
+}
 
+function updateMap(){
   for (let y = map.length - 1; y >= 0; y--) {
     for (let x = 0; x < map[y].length; x++) {
       if ((map[y][x] === Tile.STONE || map[y][x] === Tile.FALLING_STONE)
@@ -110,6 +112,11 @@ function update() {
       }
     }
   }
+}
+
+function update() {
+  handleInputs();
+  updateMap();
 }
 
 function createGraphics() {
