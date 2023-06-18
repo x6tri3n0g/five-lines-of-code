8e06cd18db602a45411281192978fb5b1917fc21
xtring <hh940630@gmail.com>
2023-06-19 01:37:09 +0900

    handleInputs() 함수에서 if문의 영역을 handleInput() 함수로 추출하라

--- index.ts
+++ index.ts
@@ -80,17 +80,21 @@ function moveVertical(dy: number) {
   }
 }
 
+function handleInput(current: Input) {
+  if (current === Input.LEFT)
+    moveHorizontal(-1);
+  else if (current === Input.RIGHT)
+    moveHorizontal(1);
+  else if (current === Input.UP)
+    moveVertical(-1);
+  else if (current === Input.DOWN)
+    moveVertical(1);
+}
+
 function handleInputs() {
   while (inputs.length > 0) {
     let current = inputs.pop();
-    if (current === Input.LEFT)
-      moveHorizontal(-1);
-    else if (current === Input.RIGHT)
-      moveHorizontal(1);
-    else if (current === Input.UP)
-      moveVertical(-1);
-    else if (current === Input.DOWN)
-      moveVertical(1);
+    handleInput(current);
   }
 }
 
